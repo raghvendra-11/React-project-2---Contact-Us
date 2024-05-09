afde0c145ef03b82497e46b77607083707b8bc7e
cse-raghvendra <60070962+cse-raghvendra@users.noreply.github.com>
2024-05-09 12:05:44 +0530

    first commit

---
+++ Contact Us/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+
+const Navigation = () => {
+    return (
+      <nav>
+          <div className="">
+              <img src="./images/logo.png" alt="logo"/>
+          </div>
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact Us</li>
+          </ul>
+      </nav>
+    )
+  }
+  
+  export default Navigation
